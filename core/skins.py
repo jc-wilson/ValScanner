0f0e5aab15d99c663df901892f3ec436b1f5a7c0
jc-wilson <97450109+jc-wilson@users.noreply.github.com.>
2026-03-06 08:03:51 +0000

    added buddy retrieval

--- core/skins.py
+++ core/skins.py
@@ -8,6 +8,7 @@ class SkinHandler:
         self.uuid_handler = UUIDHandler()
         self.uuid_handler.skin_uuid_function()
         self.converted_skins = {}
+        self.converted_buddies = {}
         self.skins = None
         self.skins_pre = None
         self._lock = asyncio.Lock()
@@ -32,6 +33,7 @@ class SkinHandler:
 
     def convert_skins(self, puuid):
         skin_uuids = []
+        buddy_uuids = []
         if self.skins:
             for player in self.skins["Loadouts"]:
                 if player["Loadout"]["Subject"] == puuid:
@@ -39,15 +41,28 @@ class SkinHandler:
                         skin_uuids.append(
                             player["Loadout"]["Items"][weapons]["Sockets"]["3ad1b2b2-acdb-4524-852f-954a76ddae0a"][
                                 "Item"]["ID"])
+                        try:
+                            buddy_uuids.append(
+                                player["Loadout"]["Items"][weapons]["Sockets"]["dd3bf334-87f3-40bd-b043-682a57a8dc3a"][
+                                    "Item"]["ID"])
+                        except KeyError:
+                            buddy_uuids.append("")
 
         elif self.skins_pre:
+            print(self.skins_pre)
             for player in self.skins_pre["Loadouts"]:
                 if player["Subject"] == puuid:
                     for weapons in player["Items"]:
                         skin_uuids.append(
                             player["Items"][weapons]["Sockets"]["3ad1b2b2-acdb-4524-852f-954a76ddae0a"]["Item"]["ID"])
+                        try:
+                            buddy_uuids.append(
+                                player["Items"][weapons]["Sockets"]["dd3bf334-87f3-40bd-b043-682a57a8dc3a"]["Item"]["ID"])
+                        except KeyError:
+                            buddy_uuids.append("")
 
         self.converted_skins[puuid] = skin_uuids
+        self.converted_buddies[puuid] = buddy_uuids
 
     async def assign_skins(self, puuid, match_uuid, match_id_header, region, shard, session):
         async with self._lock:
@@ -63,31 +78,37 @@ class SkinHandler:
             except (IndexError, KeyError):
                 return None
 
+        def get_buddy(index):
+            try:
+                return self.converted_buddies[puuid][index]
+            except (IndexError, KeyError):
+                return None
+
         return {
-            "Classic": get_skin(1),
-            "Bandit": get_skin(3),
-            "Shorty": get_skin(4),
-            "Frenzy": get_skin(5),
-            "Ghost": get_skin(0),
-            "Sheriff": get_skin(16),
+            "Classic": [get_skin(1), get_buddy(1)],
+            "Bandit": [get_skin(3), get_buddy(3)],
+            "Shorty": [get_skin(4), get_buddy(4)],
+            "Frenzy": [get_skin(5), get_buddy(5)],
+            "Ghost": [get_skin(0), get_buddy(0)],
+            "Sheriff": [get_skin(16), get_buddy(16)],
 
-            "Stinger": get_skin(19),
-            "Spectre": get_skin(6),
+            "Stinger": [get_skin(19), get_buddy(19)],
+            "Spectre": [get_skin(6), get_buddy(6)],
 
-            "Bucky": get_skin(11),
-            "Judge": get_skin(17),
+            "Bucky": [get_skin(11), get_buddy(11)],
+            "Judge": [get_skin(17), get_buddy(17)],
 
-            "Bulldog": get_skin(14),
-            "Guardian": get_skin(7),
-            "Phantom": get_skin(18),
-            "Vandal": get_skin(12),
+            "Bulldog": [get_skin(14), get_buddy(14)],
+            "Guardian": [get_skin(7), get_buddy(7)],
+            "Phantom": [get_skin(18), get_buddy(18)],
+            "Vandal": [get_skin(12), get_buddy(12)],
 
-            "Marshal": get_skin(15),
-            "Outlaw": get_skin(9),
-            "Operator": get_skin(13),
+            "Marshal": [get_skin(15), get_buddy(15)],
+            "Outlaw": [get_skin(9), get_buddy(9)],
+            "Operator": [get_skin(13), get_buddy(13)],
 
-            "Ares": get_skin(8),
-            "Odin": get_skin(10),
+            "Ares": [get_skin(8), get_buddy(8)],
+            "Odin": [get_skin(10), get_buddy(10)],
 
-            "Knife": get_skin(2),
+            "Knife": [get_skin(2), get_buddy(2)]
         }
