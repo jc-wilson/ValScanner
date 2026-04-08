f4d4b2e24ba4bc56dda4523043cea515491b5ca9
jc-wilson <97450109+jc-wilson@users.noreply.github.com.>
2026-04-08 07:31:52 +0100

    version bump

--- frontend/QApplication.py
+++ frontend/QApplication.py
@@ -63,7 +63,7 @@ from core.asset_loader import (
     load_buddy_pixmap,
 )
 
-CURRENT_VERSION = "1.10"
+CURRENT_VERSION = "1.11"
 UPDATE_CHECK_URL = "https://ValScanner.com/version.json"
 WEBSITE_URL = "https://ValScanner.com/"
 APP_INSTANCE_KEY = "ValScanner.SingleInstance"
